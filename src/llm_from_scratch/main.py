73902b01f9e0289cb9a03bc747ae12c515a82602
Matt Zhang <set.stun@gmail.com>
2023-12-01 00:40:53 -0600

    Added training function and plot

--- src/llm_from_scratch/main.py
+++ src/llm_from_scratch/main.py
@@ -1,9 +1,13 @@
+import matplotlib
+matplotlib.use('agg')
+from matplotlib import pyplot as plt
 import torch
 from torch import nn
 
 from llm_from_scratch.models import BigramLanguageModel
-from llm_from_scratch.sample_prep import get_train_validation_split, get_batch
+from llm_from_scratch.sample_prep import get_train_validation_split
 from llm_from_scratch.tokenizer import get_token_set, encode, decode
+from llm_from_scratch.training import train_model
 
 
 def generate(model: nn.Module, context: torch.Tensor, max_new_tokens: int, vocab: list[str]) -> str:
@@ -17,6 +21,14 @@ def generate(model: nn.Module, context: torch.Tensor, max_new_tokens: int, vocab
     return generated_text
 
 
+def plot_loss_history(loss_history: list[torch.Tensor], filename: str) -> None:
+    plt.plot(loss_history)
+    plt.title("Loss history")
+    plt.xlabel("Batch")
+    plt.ylabel("Loss")
+    plt.savefig(filename)
+
+
 if __name__ == "__main__":
     torch.manual_seed(1337)
     with open("data/tiny_shakespeare.txt", "r") as f:
@@ -28,10 +40,7 @@ if __name__ == "__main__":
 
     model = BigramLanguageModel(len(vocab))
 
-    x, y = get_batch(train_data, batch_size=4, block_size=8)
-    # train_with_batch(x, y)
-    # logits, loss = model(x, y)
-
+    """
     generated_text = generate(
         model=model,
         context=torch.tensor([[vocab.index("S")]]),
@@ -39,3 +48,7 @@ if __name__ == "__main__":
         vocab=vocab,
     )
     print(generated_text)
+    """
+
+    loss_history = train_model(model, train_data)
+    plot_loss_history(loss_history, "loss_history.png")
--- src/llm_from_scratch/training.py
+++ src/llm_from_scratch/training.py
@@ -3,21 +3,22 @@ import torch
 from llm_from_scratch.sample_prep import get_batch
 
 
-def train_model(model: torch.nn.Module, train_data: torch.Tensor) -> float:
+def train_model(model: torch.nn.Module, train_data: torch.Tensor) -> list[torch.Tensor]:
     optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
 
     batch_size = 32
-    n_batches = 100
+    n_batches = 10_000
 
-    loss = torch.Tensor(0.0)
+    loss_history = []
     for _ in range(n_batches):
         x, y = get_batch(train_data, batch_size, block_size=8)
         _, loss = model(x, y)
         optimizer.zero_grad(set_to_none=True)
         loss.backward()
         optimizer.step()
+        loss_history.append(loss.item())
 
-    return loss.item()
+    return loss_history
 
 
 def train_with_batch(x: torch.Tensor, y: torch.Tensor) -> None:
